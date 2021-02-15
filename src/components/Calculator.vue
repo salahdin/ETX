970d049c7382da652cd7c3bd1a2aaebe284a9545
salahdin <salahdinjemal@gmail.com>
2021-02-15 09:04:03 +0300

    fix calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,16 +1,13 @@
 <template>
-  <v-container>
+  <v-container id="app">
     <v-row>
-      <v-col md="4" sm="12" xs="12">
-        <v-text-field
-          v-model="user_input"
-          :label="select.currency"
-        ></v-text-field>
+      <v-col cols="12" md="4" sm="12" xs="12">
+        <v-text-field v-model="user_input" label="input value"></v-text-field>
       </v-col>
-      <v-col md="8" sm="12" xs="12">
+      <v-col cols="12" md="8" sm="12" xs="12">
         <v-select
           v-model="select"
-          :hint="`${select.currency},${select.name}, ${select.price_min} - ${select.price_max}`"
+          hint="select a currency you want to convert to"
           :items="newtable"
           item-text="currency"
           item-value="ETBprice"
@@ -22,55 +19,70 @@
       </v-col>
 
       <v-col cols="12" md="12" sm="12" xs="12">
-      <div class="text-center">
-        <v-dialog v-model="dialog" width="700">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn @click="getOfficialData(select.currency)" color="#101820FF" dark v-bind="attrs" v-on="on">
-              calculate
-            </v-btn>
-          </template>
-
-
-          <v-card>
-            <v-card-title class="headline grey lighten-2">
-              {{ user_input }} {{ select.currency }}
-            </v-card-title>
-
-            <v-card-title class="headline grey lighten-2">
-              <h1>=</h1>
-            </v-card-title>
-
-            <v-card-title class="headline grey lighten-2">
-              <p>black market : 
-              {{ multiply(select.price_min, user_input) }} ETB -
-              {{ multiply(select.price_max, user_input) }} ETB</p>
-              <v-spacer></v-spacer>
-              <p v-for="(value, key) in officialData" :key="key">official : {{ multiply(value.toFixed(2),user_input) }} ETB</p>
-            </v-card-title>
-            <v-divider></v-divider>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="primary" text @click="dialog = !dialog">
-                close
+        <div class="text-center">
+          <v-dialog v-model="dialog" width="700">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                @click="getOfficialData()"
+                color="#101820FF"
+                dark
+                v-bind="attrs"
+                v-on="on"
+              >
+                calculate
               </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </div>
+            </template>
+
+            <v-card>
+              <v-card-title class="headline grey lighten-2">
+                {{ user_input }} {{ select.currency }}
+              </v-card-title>
+
+              <v-card-title class="headline grey lighten-2">
+                <h1>=</h1>
+              </v-card-title>
+
+              <v-card-title class="headline grey lighten-2">
+                <div>
+                  <p>
+                    street value :
+                    {{ multiply(select.price_min, user_input) }} -
+                    {{ multiply(select.price_max, user_input) }} ETB
+                  </p>
+                </div>
+              </v-card-title>
+
+              <v-card-title class="headline grey lighten-2"> 
+                <div>
+                  official : {{ divide(user_input,official[select.currency]) }} ETB
+                </div>
+              </v-card-title>
+
+              <v-card-text> </v-card-text>
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" text @click="dialog = !dialog">
+                  close
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import axios from "axios"
+import axios from "axios";
 
 export default {
   data() {
     return {
       newtable: [],
-      select: {currency:"select a currency",name:"click"},
+      select: {currency:'ETB'},
       user_input: 0,
       airresponse: null,
       dialog: false,
@@ -80,13 +92,34 @@ export default {
   },
   mounted() {
     this.loadItems()
-    this.select = this.newtable[0]
   },
-  computed:{
-    round(value){
-      return value.toFixed(2)
+  watch: {
+    select() {
+      this.$store.commit("setSelect", this.select)
+      this.$store.dispatch('loadGraphData',this.select.currency)
+    },
+  },
+  computed: {
+    round(value) {
+      return value.toFixed(2);
+    },
+    official(){
+      return this.$store.getters.getTableData
     }
-
+  },
+  metaInfo() {
+    return {
+      title:
+        "EXbirr | birr exchange calculator with street value and official price",
+      meta: [
+        {
+          vmid: "birr converter",
+          name: "calculator",
+          content:
+            "birr exchange calculator with black market and official price",
+        },
+      ],
+    };
   },
   methods: {
     loadItems() {
@@ -98,40 +131,30 @@ export default {
           }
         )
         .then((response) => {
-          this.airresponse = response.data.records
+          this.airresponse = response.data.records;
           for (var i of response.data.records) {
             let x = {
               price_max: i.fields.price_max,
               currency: i.fields.currency,
               price_min: i.fields.price_min,
               name: i.fields.name,
-            }
-            this.newtable.push(x)
+            };
+            this.newtable.push(x);
           }
         })
         .catch((error) => {
           console.log(error);
         });
     },
-    getOfficialData(currency) {
-      axios
-        .get(
-          "https://free.currconv.com/api/v7/convert?q=" +
-            currency +
-            "_ETB&compact=ultra&apiKey=f4f228b29bb098fe82e8"
-        )
-        .then((response) => {
-          this.officialData = response.data
-          this.$store.commit('setSelect', this.select)
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-        
+    getOfficialData() {
+      this.$store.commit("setSelect", this.select)
     },
     multiply(a, b) {
-      return Math.round(a * b,1);
+      return Math.round(a * b, 1)
     },
+    divide(a, b) {
+      return Math.round(a/b, 2)
+    }
   },
 };
 </script>
---
+++ src/components/Gchart.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container id="app">
+    <v-row justify="center" align="center">
+      <v-col cols="12" md="12" sm="12">
+        <GChart
+          type="LineChart"
+          :data="GchartData"
+          :options="chartOptions"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { GChart } from "vue-google-charts"
+
+export default {
+  data: () => ({
+    chartOptions: {
+      chart: {
+        title: "street value",
+        subtitle: "street value increase",
+      },
+    },
+  }),
+  components: {
+    GChart,
+  },
+  computed: {
+    GchartData() {
+      return this.$store.getters.getChartData.Gchart 
+    }
+    
+  },
+};
+</script>
+
+<style>
+</style>

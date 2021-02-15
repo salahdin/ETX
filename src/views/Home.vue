6c82de00efb2cfe2b3d079d8872cb083d7e38c29
salahdin <salahdinjemal@gmail.com>
2021-02-15 09:02:11 +0300

    add firebase hosting config

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-   
-  </div>
+  <v-app>
+    <Gchart></Gchart>
+    <Calculator></Calculator>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
+import Calculator from '../components/Calculator'
+import Gchart from '../components/Gchart'
+
+
 
 export default {
   name: 'Home',
   components: {
-  
-  }
+    Calculator,
+    Gchart,
+  },
+  metaInfo() {
+  return {
+    title:
+      "Welcome to Exbirr",
+    meta: [
+      {
+        vmid: "birr converter | blackmarket and official value",
+        name: "exbirr",
+        content:
+        "birr exchange calculator with street value ( blackmarket ) and official price",
+
+      },
+    ],
+  };
+}
 }
 </script>

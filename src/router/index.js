71a87c8c392806bb515376bf6c216443e84f314b
salahdin <salahdinjemal@gmail.com>
2021-02-15 09:03:45 +0300

    add meta info for seo

--- src/router/index.js
+++ src/router/index.js
@@ -9,13 +9,31 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
-    meta: {title:"birr exchange calculator with black market and official prices ", auth: false}
+    meta: {title:"birr exchange calculator with street value and official prices ", auth: false}
   },
   {
     path: '/calculator',
     name: 'Calculator',
     component: () => import('../components/Calculator.vue'),
     meta: {title:"birr exchange calculator ", auth: false}
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/About'),
+    meta: {title:"birr exchange calculator ", auth: false}
+  },
+  {
+    path: '/table',
+    name: 'table',
+    component: () => import('../components/table.vue'),
+    meta: {title:"table ", auth: false}
+  },
+  {
+    path: '/chartt',
+    name: 'chart',
+    component: () => import('../components/Gchart.vue'),
+    meta: {title:"chart ", auth: false}
   }
 ]
 
